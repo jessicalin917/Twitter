ed2b9e58d0f9000d7de63a8630458ead343ed016
Yu Chieh Lin <jessicalin917@gmail.com>
2019-03-13 23:59:42 -0700

    ddhhj

--- Twitter/HomeTableTableViewController.swift
+++ Twitter/HomeTableTableViewController.swift
@@ -70,7 +70,7 @@ class HomeTableTableViewController: UITableViewController {
         
         cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
         cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
-        cell.setSelected = tweetArray[indexPath.row]["retweeted"] as! Bool
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
     }
